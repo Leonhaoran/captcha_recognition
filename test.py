18e1d888285fb516c2985b69f9ebf7e7fd70b253
22373400 <22373400@buaa.edu.cn>
2026-05-03 13:20:00 +0800

    测试

--- test.py
+++ test.py
@@ -1,10 +1,48 @@
+from dataset import CaptchaDataset
+from model import CNNModel
+
 import torch
-import torch.nn as nn
+from torch.utils.data import DataLoader
+from torchvision import transforms
 
-input = torch.randn(1, 1, 6, 6)
-print(input)
+import json
 
-output = nn.Dropout(0.25)(input)
+if __name__ == "__main__":
+    with open("config.json", "r") as f:
+        config = json.load(f)
 
-print("\n" + 50 * "-" + "\n")
-print(output)
+    height = config["resize_height"]
+    width = config["resize_width"]
+
+    transform = transforms.Compose([
+        transforms.Resize((height, width)),
+        transforms.ToTensor()])
+
+    test_data_path = config["test_data_path"]
+    characters = config["characters"]
+    digit_num = config["digit_num"]
+    class_num = len(characters) * digit_num
+    test_model_path = config["test_model_path"]
+
+    test_data = CaptchaDataset(test_data_path, transform, characters)
+    test_load = DataLoader(test_data, batch_size=1, shuffle=False)
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model = CNNModel(height, width, digit_num, class_num).to(device)
+    model.eval()
+    model.load_state_dict(torch.load(test_model_path, map_location=device))
+
+    correct = 0
+    total = 0
+
+    for (x, y) in test_load:
+        x, y = x.to(device), y.to(device)
+
+        pred = model(x)
+
+        if torch.equal(pred.argmax(dim=2).squeeze(0), y.squeeze(0)):
+            correct += 1
+        total += 1
+
+    acc = correct / total
+    print("Test accuracy: {:.2f}%".format(acc * 100))
